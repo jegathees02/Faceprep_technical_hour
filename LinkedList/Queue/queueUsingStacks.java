f26dab77005c2d73a5635121aa0aaafcc4730ff7
jegathees02 <100774735+jegathees02@users.noreply.github.com>
2024-02-02 09:06:31 +0530

    commit

--- LinkedList/Queue/queueUsingStacks.java
+++ LinkedList/Queue/queueUsingStacks.java
@@ -2,6 +2,30 @@ package LinkedList.Queue;
 
 import java.util.*;
 public class queueUsingStacks {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    public void enqueue(int data) {
+        stack1.push(data);
+    }
+    public void dequeue() {
+        while(!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack2.pop();
+        while(!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+    public void print() {
+        while(!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        while(!stack2.isEmpty()) {
+            System.out.print(stack2.peek()+" - ");
+            stack1.push(stack2.pop());
+        }
+        System.out.println();
+    }
     public void operations() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the choice:");
@@ -9,20 +33,28 @@ public class queueUsingStacks {
         System.out.println("2:Dequeue");
         int inp = sc.nextInt();
         if(inp == 1) {
-
+            System.out.println("Enter value to add");
+            int data = sc.nextInt();
+            enqueue(data);
+            print();
+            operations();
         }
         else if(inp == 2)  {
-            
+            dequeue();
+            print();
+            operations();
         }
         else{
+            System.out.println("Try Again");
             operations();
         }
         sc.close();
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Stack<Integer> stack1 = new Stack<Integer>();
-        Stack<Integer> stack2 = new Stack<Integer>();
+        queueUsingStacks q = new  queueUsingStacks();
+        q.operations();
+        // Scanner sc = new Scanner(System.in);
+        
 
         
     }
--- LinkedList/stack/infixToPostfix_Jegathees.java
+++ LinkedList/stack/infixToPostfix_Jegathees.java
@@ -2,9 +2,16 @@ package LinkedList.stack;
 
 import java.util.*;
 public class infixToPostfix_Jegathees {
+    public int prec(char ch) {
+        if(ch == '^') return 3;
+        else if(ch == '*' || ch == '/') return 2;
+        else if(ch == '-' || ch == '+') return 1;
+        else return -1;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        infixToPostfix_Jegathees itp = new infixToPostfix_Jegathees();
         Stack<Character> stack = new Stack<Character>();
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<str.length();i++) {
@@ -22,8 +29,18 @@ public class infixToPostfix_Jegathees {
                 stack.pop();
             }
             else{
-                
+                while(!stack.isEmpty() && 
+                (itp.prec(ch) <= itp.prec(stack.peek()))
+                ) {
+                    sb.append(stack.pop());
+                }
+                stack.push(ch);
             }
+
         }
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        System.out.println(sb.toString());
     }
 }
